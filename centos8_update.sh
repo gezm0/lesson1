cdf055fa9a584bcb40d762339c94ca92648da062
gezm0 <eremin@ryazan.net.ru>
2022-02-01 10:42:21 +0300

    script for centos8 update

---
+++ centos8_update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+
+dnf update -y
